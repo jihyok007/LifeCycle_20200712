cad3c4354b7796b69cb45b1e5536f7b2ae760844
최지혁 <jihyok007@gmail.com>
2020-07-12 12:09:18 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200712/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.lifecycle_20200712
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity {
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
